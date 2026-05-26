dd908e5fcb1069f39afb90bcee9a7e9f2b4e63d0
Ahnaf Faisal <ahnaffaisalalavi@gmail.com>
2026-05-25 23:43:39 -0700

    Initial commit

---
+++ src/main/java/com/example/leet75/Leet75Application.java
@@ -0,0 +1,13 @@
+package com.example.leet75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leet75Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Leet75Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leet75/Leet75ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leet75;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Leet75ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
